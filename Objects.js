51636c518858a5e8408e41b1909d8c150075e25b
P H G Macedo <git@phgmacedo.com>
2020-10-03 22:33:28 -0300

    different types of object declarations

--- Objects.js
+++ Objects.js
@@ -1,6 +1,20 @@
+// constructor
 function Rectangle(a, b) {
     this.length = a
     this.width = b
     this.perimeter = 2 * (a + b)
     this.area = a * b
 }
+
+var rect = {
+    length: 2
+    width: 4
+}
+
+rect.area = 2 * 8
+
+var rect2 = new Object()
+var rect3 = Object.create()
+
+
+// instantiation: new Rectangle(2, 4)
