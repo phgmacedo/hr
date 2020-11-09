6b4771a5db017fb67f0f51d71659cd690e223c94
P H G Macedo <git@phgmacedo.com>
2020-11-09 17:13:48 -0300

    fixed runner up

--- python/find-the-runner-up-score.py
+++ python/find-the-runner-up-score.py
@@ -1,9 +1,9 @@
 n = int(input())
 # once an iterator has been exhausted, there nothing left to be iterated over
 # iterating it a second time won't yield anything
-arr = map(int, input().split())
+arr = list(map(int, input().split()))
 
 runnerUp = [item for item in list(arr) if item < max(list(arr))]
 
-# this is slow code
+# this is bad code
 print(max(runnerUp))
