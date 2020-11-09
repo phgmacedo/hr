bcd4a8f14a1602bd9c7d3297292bf602dbd1a0f0
P H G Macedo <git@phgmacedo.com>
2020-11-09 20:47:13 -0300

    remove if from list-lambdas

--- python/lists-lambdas.py
+++ python/lists-lambdas.py
@@ -5,14 +5,13 @@ commands = {"insert": lambda li, args: li.insert(*args),
             "append": lambda li, args: li.append(*args),
             "sort": lambda li, args: li.sort(*args),
             "pop": lambda li, args: li.pop(*args),
-            "reverse": lambda li, args: li.reverse(*args)}
+            "reverse": lambda li, args: li.reverse(*args),
+            "print": lambda li, args: print(li)
+            }
 
 li = []
 args = []
 for _ in range(n):
     command, *line = input().split()
     args = list(map(int, line))
-    if command != "print":
-        commands[command](li, args)
-    else:
-        print(li)
+    commands[command](li, args)
