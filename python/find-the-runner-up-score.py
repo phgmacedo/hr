01d09b5f34f1507574eb0ae49aae54b2c46b8203
P H G Macedo <git@phgmacedo.com>
2020-11-09 17:19:00 -0300

    mine was gooder

--- python/find-the-runner-up-score.py
+++ python/find-the-runner-up-score.py
@@ -1,12 +1,8 @@
 n = int(input())
-# once an iterator has been exhausted, there nothing left to be iterated over
-# iterating it a second time won't yield anything
 arr = list(map(int, input().split()))
 
-z = max(arr)
-
-while max(arr) == z:
-    arr.remove(max(arr))
+# there are 2 maxes
+m = max([x for x in arr if x < max(arr)])
 
 # this is bad code
-print(max(arr))
+print(m)
