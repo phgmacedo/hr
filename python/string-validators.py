f5d0871893fe19a5ce90df030cd12e752d21f7d0
P H G Macedo <git@phgmacedo.com>
2020-11-29 18:02:47 -0300

    optimize loops iteration, alocate one less

--- python/string-validators.py
+++ python/string-validators.py
@@ -1,36 +1,43 @@
 if __name__ == '__main__':
     s = input()
 
-# a = False
-# for l in s:
-#     if l.isalnum():
-#         a = True
+a = False
+for l in s:
+    if l.isalnum():
+        a = True
+        break
 
-# b = False
-# for l in s:
-#     if l.isalpha():
-#         b = True
+b = False
+for l in s:
+    if l.isalpha():
+        b = True
+        break
 
-# c = False
-# for l in s:
-#     if l.isdigit():
-#         c = True
+c = False
+for l in s:
+    if l.isdigit():
+        c = True
+        break
 
-# d = False
-# for l in s:
-#     if l.islower():
-#         d = True
+d = False
+for l in s:
+    if l.islower():
+        d = True
+        break
 
-# e = False
-# for l in s:
-#     if l.isupper():
-#         e = True
+e = False
+for l in s:
+    if l.isupper():
+        e = True
+        break
+
+print(*[a, b, c, d, e], sep="\n")
 
 # for i in [a, b, c, d, e]:
 #     print(i)
 
-print(any(c.isalnum() for c in s))
-print(any(c.isalpha() for c in s))
-print(any(c.isdigit() for c in s))
-print(any(c.islower() for c in s))
-print(any(c.isupper() for c in s))
+# print(any(c.isalnum() for c in s))
+# print(any(c.isalpha() for c in s))
+# print(any(c.isdigit() for c in s))
+# print(any(c.islower() for c in s))
+# print(any(c.isupper() for c in s))
