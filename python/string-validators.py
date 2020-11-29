d6da597c2e29c4a757745e92dcdf8b15ffa214bd
P H G Macedo <git@phgmacedo.com>
2020-11-29 17:58:02 -0300

    uses any()

--- python/string-validators.py
+++ python/string-validators.py
@@ -1,30 +1,36 @@
 if __name__ == '__main__':
     s = input()
 
-a = False
-for l in s:
-    if l.isalnum():
-        a = True
+# a = False
+# for l in s:
+#     if l.isalnum():
+#         a = True
 
-b = False
-for l in s:
-    if l.isalpha():
-        b = True
+# b = False
+# for l in s:
+#     if l.isalpha():
+#         b = True
 
-c = False
-for l in s:
-    if l.isdigit():
-        c = True
+# c = False
+# for l in s:
+#     if l.isdigit():
+#         c = True
 
-d = False
-for l in s:
-    if l.islower():
-        d = True
+# d = False
+# for l in s:
+#     if l.islower():
+#         d = True
 
-e = False
-for l in s:
-    if l.isupper():
-        e = True
+# e = False
+# for l in s:
+#     if l.isupper():
+#         e = True
 
-for i in [a, b, c, d, e]:
-    print(i)
+# for i in [a, b, c, d, e]:
+#     print(i)
+
+print(any(c.isalnum() for c in s))
+print(any(c.isalpha() for c in s))
+print(any(c.isdigit() for c in s))
+print(any(c.islower() for c in s))
+print(any(c.isupper() for c in s))
