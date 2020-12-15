521c650dc4aea0d8f658120b4d1b7b8f28c3abbf
P H G Macedo <git@phgmacedo.com>
2020-12-14 23:02:21 -0300

    text wrap

---
+++ python/text-wrap.py
@@ -0,0 +1,17 @@
+import textwrap
+
+
+def wrap(string, max_width):
+    n = int(len(string)/max_width)+1
+
+    wrapped_text = str()
+    for i in range(n):
+        paragraph = slice(i*max_width, (i+1)*max_width)
+        wrapped_text += string[paragraph] + "\n"
+    return wrapped_text
+
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
