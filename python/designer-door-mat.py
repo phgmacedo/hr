6906403c9fd5827a9e42817d055f005460c73bb2
P H G Macedo <git@phgmacedo.com>
2021-03-03 19:32:19 -0300

    designer door mat

---
+++ python/designer-door-mat.py
@@ -0,0 +1,22 @@
+n, m = list(map(int, input().split()))
+
+dick = ".|."
+dash = "-"
+wel = "WELCOME"
+
+middle = int((m-7)/2)*dash+wel+int((m-7)/2)*dash
+
+# k is the number of iterations on each half without the welcome
+for k in range(int((n - 1) / 2)):
+    # p is the num of dicks
+    p = (2 * k + 1)
+    # l is the number of dashes before dicks
+    l = int((m-3*p)/2)
+    print(l * dash + (p) * dick + l * dash)
+
+print(middle)
+
+for k in range(int((n - 1) / 2)-1, -1, -1):
+    p = (2*k+1)
+    l = int((m-3*p)/2)
+    print(l*dash+(p)*dick+l*dash)
