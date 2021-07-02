582f6e285b3f44193eee81c8286faa7f42e19ee4
P H G Macedo <git@phgmacedo.com>
2021-07-01 23:17:01 -0300

    interview kit

---
+++ python/left-rotation.py
@@ -0,0 +1,36 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER d
+#
+
+
+def rotLeft(a, d):
+    n = len(a)
+    for i in range(n):
+        el = a[(i-d) % n]
+        # print(el, end=" ")
+        print((i-d) % n)
+    return 0
+
+
+if __name__ == '__main__':
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
